91a631453cf76ea7100773f4eb47ef1bb0d33177
Selfbotman <34406024+Selfbotman@users.noreply.github.com>
2018-02-10 00:27:09 +0000

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/7.14.0"
-    LA = "IOSIPAD\t7.14.0\tiPhone OS\t10.12.0"
+    UA = "Line/8.0.1"
+    LA = "DESKTOPMAC\t10.10.2-YOSEMITE-x64\tMAC 4.5.0"
 
     authToken = None
     mid = None
